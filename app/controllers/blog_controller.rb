69fa6af54e85d12a625abb63fd34350d169ac537
peter ji <hyeongjun9@gmail.com>
2023-08-23 16:46:43 -0700

    completed challenges

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -24,6 +24,17 @@ class BlogController < ApplicationController
     redirect_to blogs_path
   end 
   
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+    redirect_to blogs_path
+    end
+  end
+
   private 
   def blog_params
     params.require(:blog).permit(:title, :content)
